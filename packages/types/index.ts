d9d8ab3682d59e4b1512b09f12d0fc2164099fd2
arpansaha13 <arpansaha1300@gmail.com>
2024-05-19 00:46:23 +0530

    fix: add missing types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,3 +1,7 @@
 export * from './client.types'
 export * from './message.types'
 export * from './response.types'
+
+export interface RequestOptions extends Omit<RequestInit, 'body'> {
+  body: Record<string, any>
+}
