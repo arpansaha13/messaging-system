4548aa0718efcc1849dfe177dd71a7599915fae2
arpansaha13 <arpansaha1300@gmail.com>
2024-05-19 15:52:22 +0530

    fix: types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -3,5 +3,5 @@ export * from './message.types'
 export * from './response.types'
 
 export interface RequestOptions extends Omit<RequestInit, 'body'> {
-  body: Record<string, any>
+  body?: Record<string, any>
 }
