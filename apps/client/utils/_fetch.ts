7e167ba870516412122132051fb0c6f5e3d5734b
arpansaha13 <arpansaha1300@gmail.com>
2024-05-26 15:22:09 +0530

    refactor: remove unused utils

--- apps/client/utils/_fetch.ts
+++ apps/client/utils/_fetch.ts
@@ -1,4 +1,3 @@
-import createRequest from './createRequest'
 import type { RequestOptions } from '@pkg/types'
 
 export default async function _fetch(url: string, options?: RequestOptions) {
@@ -18,4 +17,26 @@ export default async function _fetch(url: string, options?: RequestOptions) {
 
   if (res.status >= 400) throw jsonData
   return jsonData
+}
+
+export function createRequest(url: string, options?: RequestOptions) {
+  const headers = {
+    ...(options?.headers ?? {}),
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
+
+  const body = options?.body ? new URLSearchParams(options?.body).toString() : null
+
+  if (url.startsWith('/')) {
+    url = '/api' + url
+  } else {
+    url = '/api/' + url
+  }
+
+  return new Request(url, {
+    ...options,
+    body,
+    headers,
+    credentials: 'include',
+  })
 }
