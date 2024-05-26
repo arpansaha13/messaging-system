68a1345d3a750f3fb96f601f00fed2bfbcbbca56
arpansaha13 <arpansaha1300@gmail.com>
2024-05-26 15:14:29 +0530

    refactor: convo item

--- apps/client/app/(layout-wrapper)/(chats)/archived/page.tsx
+++ apps/client/app/(layout-wrapper)/(chats)/archived/page.tsx
@@ -1,10 +1,20 @@
 'use client'
 
 import { shallow } from 'zustand/shallow'
-import ConvoItem from '~/components/Convo/ConvoItem'
+import ConvoItemTemplate from '~/components/Convo/ConvoItemTemplate'
+import ConvoItemDropDown from '~/components/Convo/ConvoItemDropDown'
 import { useStore } from '~/store'
 import _fetch from '~/utils/_fetch'
 import type { ConvoItemType } from '@pkg/types'
+
+interface ArchivedConvoItemProps {
+  userId: number
+  alias: string | null
+  dp: string | null
+  globalName: string
+  latestMsg: ConvoItemType['latestMsg']
+  onClick: () => void
+}
 
 export default function Page() {
   const [archived, setActiveChat] = useStore(state => [state.archived, state.setActiveChat], shallow)
@@ -16,22 +26,58 @@ export default function Page() {
   }
 
   return (
-    <div>
-      <ul>
-        {archived.map(convoItem => (
-          <ConvoItem
-            key={convoItem.receiver.id}
-            userId={convoItem.receiver.id}
-            dp={convoItem.receiver.dp}
-            globalName={convoItem.receiver.globalName}
-            alias={convoItem.contact?.alias ?? null}
-            latestMsg={convoItem.latestMsg}
-            archived={true}
-            pinned={false}
-            onClick={() => handleClick(convoItem)}
-          />
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {archived.map(convoItem => (
+        <ArchivedConvoItem
+          key={convoItem.receiver.id}
+          userId={convoItem.receiver.id}
+          dp={convoItem.receiver.dp}
+          globalName={convoItem.receiver.globalName}
+          alias={convoItem.contact?.alias ?? null}
+          latestMsg={convoItem.latestMsg}
+          onClick={() => handleClick(convoItem)}
+        />
+      ))}
+    </ul>
+  )
+}
+
+function ArchivedConvoItem({ userId, ...remainingProps }: ArchivedConvoItemProps) {
+  const [activeChat, setActiveChat, unarchiveRoom, deleteChat, deleteConvo] = useStore(
+    state => [
+      // Initially no rooms would be active - so `activeChat` may be null
+      state.activeChat,
+      state.setActiveChat,
+      state.unarchiveRoom,
+      state.deleteChat,
+      state.deleteConvo,
+    ],
+    shallow,
+  )
+
+  const menuItems = [
+    {
+      slot: 'Unarchive chat',
+      onClick() {
+        unarchiveRoom(userId)
+      },
+    },
+    {
+      slot: 'Delete chat',
+      onClick() {
+        deleteChat(userId)
+        deleteConvo(userId, true)
+        // If active room is being deleted
+        if (activeChat && activeChat.receiver.id === userId) {
+          setActiveChat(null)
+        }
+      },
+    },
+  ]
+
+  return (
+    <ConvoItemTemplate {...remainingProps} userId={userId}>
+      <ConvoItemDropDown menuItems={menuItems} />
+    </ConvoItemTemplate>
   )
 }
--- apps/client/app/(layout-wrapper)/(chats)/page.tsx
+++ apps/client/app/(layout-wrapper)/(chats)/page.tsx
@@ -1,10 +1,23 @@
 'use client'
 
 import { shallow } from 'zustand/shallow'
-import ConvoItem from '~/components/Convo/ConvoItem'
+import { Icon } from '@iconify/react'
+import pinIcon from '@iconify-icons/mdi/pin'
+import ConvoItemTemplate from '~/components/Convo/ConvoItemTemplate'
+import ConvoItemDropDown from '~/components/Convo/ConvoItemDropDown'
 import { useStore } from '~/store'
 import _fetch from '~/utils/_fetch'
 import type { ConvoItemType } from '@pkg/types'
+
+export interface UnarchivedConvoItemProps {
+  userId: number
+  alias: string | null
+  dp: string | null
+  globalName: string
+  latestMsg: ConvoItemType['latestMsg']
+  pinned?: boolean
+  onClick: () => void
+}
 
 export default function Page() {
   const [convo, setActiveChat] = useStore(state => [state.unarchived, state.setActiveChat], shallow)
@@ -16,21 +29,71 @@ export default function Page() {
   }
 
   return (
-    <div>
-      <ul>
-        {convo.map(convoItem => (
-          <ConvoItem
-            key={convoItem.chat.id}
-            userId={convoItem.receiver.id}
-            dp={convoItem.receiver.dp}
-            globalName={convoItem.receiver.globalName}
-            pinned={convoItem.chat.pinned}
-            alias={convoItem.contact?.alias ?? null}
-            latestMsg={convoItem.latestMsg}
-            onClick={() => handleClick(convoItem)}
-          />
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {convo.map(convoItem => (
+        <UnarchivedConvoItem
+          key={convoItem.chat.id}
+          userId={convoItem.receiver.id}
+          dp={convoItem.receiver.dp}
+          globalName={convoItem.receiver.globalName}
+          pinned={convoItem.chat.pinned}
+          alias={convoItem.contact?.alias ?? null}
+          latestMsg={convoItem.latestMsg}
+          onClick={() => handleClick(convoItem)}
+        />
+      ))}
+    </ul>
+  )
+}
+
+function UnarchivedConvoItem({ userId, pinned = false, ...remainingProps }: UnarchivedConvoItemProps) {
+  const [activeChat, setActiveChat, archiveRoom, deleteChat, deleteConvo, updateConvoPin] = useStore(
+    state => [
+      // Initially no rooms would be active - so `activeRoom` may be null
+      state.activeChat,
+      state.setActiveChat,
+      state.archiveRoom,
+      state.deleteChat,
+      state.deleteConvo,
+      state.updateConvoPin,
+    ],
+    shallow,
+  )
+
+  const menuItems = [
+    {
+      slot: 'Archive chat',
+      onClick() {
+        archiveRoom(userId)
+      },
+    },
+    {
+      slot: 'Delete chat',
+      onClick() {
+        deleteChat(userId)
+        deleteConvo(userId, false)
+        // If active room is being deleted
+        if (activeChat && activeChat.receiver.id === userId) {
+          setActiveChat(null)
+        }
+      },
+    },
+    {
+      slot: !pinned ? 'Pin chat' : 'Unpin chat',
+      onClick() {
+        if (!pinned) {
+          updateConvoPin(userId, true)
+        } else {
+          updateConvoPin(userId, false)
+        }
+      },
+    },
+  ]
+
+  return (
+    <ConvoItemTemplate {...remainingProps} userId={userId}>
+      {pinned && <Icon icon={pinIcon} color="inherit" width={20} height={20} />}
+      <ConvoItemDropDown menuItems={menuItems} />
+    </ConvoItemTemplate>
   )
 }
