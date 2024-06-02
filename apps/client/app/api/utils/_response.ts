76c7f4d85801af902f10fc6a65d710e5d1f9c530
arpansaha13 <arpansaha1300@gmail.com>
2024-06-02 19:01:48 +0530

    fix: append Set-Cookie headers from backend response

--- apps/client/app/api/utils/_response.ts
+++ apps/client/app/api/utils/_response.ts
@@ -1,12 +1,20 @@
 import rfetch from '~api/utils/rfetch'
 
+interface Options {
+  status: number
+  statusText: string
+  data: any
+  setCookie: string[]
+}
+
 export default async function _response(request: Request) {
   const res = await rfetch(request)
 
   const { status, statusText } = res
+  const setCookie = res.headers.getSetCookie()
 
   if (status === 204 || res.body === null) {
-    return new Response(null, { status, statusText })
+    return createResponse({ data: null, status, statusText, setCookie })
   }
 
   let data = await res.text()
@@ -14,8 +22,30 @@ export default async function _response(request: Request) {
   try {
     data = JSON.parse(data)
   } catch {
-    return new Response(data, { status, statusText }) // text data
+    return createResponse({ data, status, statusText, setCookie }) // text data
   }
 
-  return Response.json(data, { status, statusText })
+  return createResponse({ data, status, statusText, setCookie })
+}
+
+function createResponse(options: Options): Response {
+  const { data, setCookie, status, statusText } = options
+
+  const responseInit: ResponseInit = { status, statusText }
+
+  if (setCookie.length > 0) {
+    const headers = new Headers()
+
+    for (const cookie of setCookie) {
+      headers.append('Set-Cookie', cookie)
+    }
+
+    responseInit.headers = headers
+  }
+
+  if (data === null || typeof data === 'string') {
+    return new Response(data, responseInit)
+  }
+
+  return Response.json(data, responseInit)
 }
