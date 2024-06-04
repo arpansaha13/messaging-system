b219f059356946901e8ad268a04e89c6793471fa
arpansaha13 <arpansaha1300@gmail.com>
2024-06-04 22:29:43 +0530

    refactor: bring context-menu handlers inside ContextMenuWrapper

--- apps/client/components/common/ContextMenu.tsx
+++ apps/client/components/common/ContextMenu.tsx
@@ -1,10 +1,17 @@
-import { Fragment } from 'react'
+import { createContext, Fragment, useContext, useState } from 'react'
 import { Menu, MenuItem, Transition } from '@headlessui/react'
 import type { IContextMenuItem } from '@pkg/types'
 
+interface IContextMenuHandlers {
+  onBlur: () => void
+  onContextMenu: (e: React.MouseEvent) => void
+}
+
+interface ContextMenuWrapperProps {
+  children: (handlers: IContextMenuHandlers) => React.ReactNode
+}
+
 interface ContextMenuProps {
-  open: boolean
-  position: Record<'top' | 'left', number>
   items: IContextMenuItem[]
 
   /**
@@ -14,8 +21,53 @@ interface ContextMenuProps {
   payload?: any
 }
 
-export default function ContextMenu(props: Readonly<ContextMenuProps>) {
-  const { open, position, items, payload } = props
+interface IContextMenuContext {
+  open: boolean
+  position: Record<'top' | 'left', number>
+}
+
+const ContextMenuContext = createContext<IContextMenuContext | null>(null)
+
+export function ContextMenuWrapper({ children }: Readonly<ContextMenuWrapperProps>) {
+  const [open, setOpen] = useState(false)
+  const [position, setPosition] = useState({ top: 0, left: 0 })
+
+  const DROPDOWN_WIDTH = 192
+
+  function onContextMenu(e: React.MouseEvent) {
+    e.preventDefault()
+
+    const rect = e.currentTarget.getBoundingClientRect()
+    let left = e.clientX - rect.left
+    let top = e.clientY - rect.top
+
+    // Handle overflow to the right
+    {
+      const dropDownRight = DROPDOWN_WIDTH + e.clientX
+
+      if (dropDownRight > rect.right) {
+        const excess = dropDownRight - rect.right
+        left -= excess
+      }
+    }
+    // TODO: handle overflow at the bottom
+
+    setOpen(true)
+    setPosition({ top, left })
+  }
+
+  function onBlur() {
+    setOpen(false)
+  }
+
+  const handlers = { onBlur, onContextMenu }
+
+  return <ContextMenuContext.Provider value={{ open, position }}>{children(handlers)}</ContextMenuContext.Provider>
+}
+
+export function ContextMenu(props: Readonly<ContextMenuProps>) {
+  const { items, payload } = props
+  const { open, position } = useContext(ContextMenuContext)!
 
   return (
     <Menu>
@@ -40,6 +92,7 @@ export default function ContextMenu(props: Readonly<ContextMenuProps>) {
               <MenuItem
                 key={i}
                 as="button"
+                type="button"
                 className="block w-full px-6 py-2.5 text-sm text-left rounded hover:bg-gray-200 dark:hover:bg-gray-900/70 transition-colors"
                 onClick={e => item.onClick(e, payload)}
               >
