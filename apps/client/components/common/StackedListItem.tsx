8e1a3636b1ad0d933787a2166a0a93d1157716ff
arpansaha13 <arpansaha1300@gmail.com>
2024-06-04 22:29:43 +0530

    refactor: bring context-menu handlers inside ContextMenuWrapper

--- apps/client/components/common/StackedListItem.tsx
+++ apps/client/components/common/StackedListItem.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { isNullOrUndefined } from '@arpansaha13/utils'
 import Avatar from '~common/Avatar'
-import ContextMenu from '~common/ContextMenu'
+import { ContextMenu, ContextMenuWrapper } from '~common/ContextMenu'
 import type { IContextMenuItem } from '@pkg/types'
 
 interface StackedListItemProps {
@@ -18,69 +18,60 @@ interface StackedListItemProps {
   payload?: any
 }
 
+interface StackedListItemButtonProps extends Pick<StackedListItemProps, 'image' | 'title' | 'subtitle' | 'text'> {
+  eventHandlers: {
+    onClick: () => void
+    onBlur?: () => void
+    onContextMenu?: (e: React.MouseEvent) => void
+  }
+}
+
 export default function StackedListItem(props: Readonly<StackedListItemProps>) {
-  const { image, title, subtitle, text, menuItems, payload, onClick } = props
+  const { menuItems, payload, onClick, ...remainingProps } = props
 
-  const [open, setOpen] = useState(false)
-  const [position, setPosition] = useState({ top: 0, left: 0 })
-
-  const DROPDOWN_WIDTH = 192
-
-  const eventHandlers = {
-    onClick,
-
-    // Register these handlers only if needed
-    ...(menuItems && {
-      onBlur: () => {
-        setOpen(false)
-      },
-      onContextMenu: (e: React.MouseEvent) => {
-        e.preventDefault()
-
-        const rect = e.currentTarget.getBoundingClientRect()
-        let left = e.clientX - rect.left
-        let top = e.clientY - rect.top
-
-        // Handle overflow to the right
-        {
-          const dropDownRight = DROPDOWN_WIDTH + e.clientX
-
-          if (dropDownRight > rect.right) {
-            const excess = dropDownRight - rect.right
-            left -= excess
-          }
-        }
-        // TODO: handle overflow at the bottom
-
-        setOpen(true)
-        setPosition({ top, left })
-      },
-    }),
+  if (isNullOrUndefined(menuItems)) {
+    return (
+      <li className="relative">
+        <StackedListItemButton {...remainingProps} eventHandlers={{ onClick }} />
+      </li>
+    )
   }
 
   return (
-    <li className="relative">
-      <button
-        className="px-3 w-full text-left hover:bg-gray-200/80 dark:hover:bg-gray-600/40 flex items-center rounded transition-colors"
-        {...eventHandlers}
-      >
-        <Avatar src={image} />
+    <ContextMenuWrapper>
+      {({ onBlur, onContextMenu }) => (
+        <li className="relative">
+          <StackedListItemButton {...remainingProps} eventHandlers={{ onBlur, onClick, onContextMenu }} />
 
-        <div className="ml-4 py-3 w-full">
-          <div className="flex justify-between">
-            <p className="text-base text-black dark:text-gray-50">{title}</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="text-sm text-gray-500 dark:text-gray-400 italic">{subtitle}</p>
-          </div>
-          <div className="mt-1 flex justify-between">
-            {/* TODO: Make a slot for icons */}
-            <p className="flex items-center text-sm text-gray-500 dark:text-gray-400 space-x-1 line-clamp-1">{text}</p>
-          </div>
+          <ContextMenu payload={payload} items={menuItems} />
+        </li>
+      )}
+    </ContextMenuWrapper>
+  )
+}
+
+function StackedListItemButton(props: Readonly<StackedListItemButtonProps>) {
+  const { image, title, subtitle, text, eventHandlers } = props
+
+  return (
+    <button
+      className="px-3 w-full text-left hover:bg-gray-200/80 dark:hover:bg-gray-600/40 flex items-center rounded transition-colors"
+      {...eventHandlers}
+    >
+      <Avatar src={image} />
+
+      <div className="ml-4 py-3 w-full">
+        <div className="flex justify-between">
+          <p className="text-base text-black dark:text-gray-50">{title}</p>
         </div>
-      </button>
-
-      {menuItems && <ContextMenu open={open} position={position} payload={payload} items={menuItems} />}
-    </li>
+        <div className="flex justify-between">
+          <p className="text-sm text-gray-500 dark:text-gray-400 italic">{subtitle}</p>
+        </div>
+        <div className="mt-1 flex justify-between">
+          {/* TODO: Make a slot for icons */}
+          <p className="flex items-center text-sm text-gray-500 dark:text-gray-400 space-x-1 line-clamp-1">{text}</p>
+        </div>
+      </div>
+    </button>
   )
 }
