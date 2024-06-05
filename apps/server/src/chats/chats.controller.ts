5badaca68f4eca545d647b427a4affe1d8705517
arpansaha13 <arpansaha1300@gmail.com>
2024-06-05 19:15:01 +0530

    refactor: return 204 response for archive/unarchive, pin/unpin, clear/delete

--- apps/server/src/chats/chats.controller.ts
+++ apps/server/src/chats/chats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Req, UseGuards } from '@nestjs/common'
+import { Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Req, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { ChatsService } from './chats.service'
 import { ChatIdParam } from './dtos/chat-id-param.dto'
@@ -22,31 +22,37 @@ export class ChatsController {
   }
 
   @Patch('/:receiverId/archive')
+  @HttpCode(HttpStatus.NO_CONTENT)
   archiveRoom(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
     return this.chatService.updateArchive(request.user.id, params.receiverId, true)
   }
 
   @Patch('/:receiverId/unarchive')
+  @HttpCode(HttpStatus.NO_CONTENT)
   unarchiveRoom(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
     return this.chatService.updateArchive(request.user.id, params.receiverId, false)
   }
 
   @Patch('/:receiverId/pin')
+  @HttpCode(HttpStatus.NO_CONTENT)
   pinChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
     return this.chatService.updatePin(request.user.id, params.receiverId, true)
   }
 
   @Patch('/:receiverId/unpin')
+  @HttpCode(HttpStatus.NO_CONTENT)
   unpinChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
     return this.chatService.updatePin(request.user.id, params.receiverId, false)
   }
 
   @Delete('/:receiverId/clear')
+  @HttpCode(HttpStatus.NO_CONTENT)
   clearChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
     return this.chatService.clearChat(request.user.id, params.receiverId)
   }
 
   @Delete('/:receiverId/delete')
+  @HttpCode(HttpStatus.NO_CONTENT)
   deleteChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
     return this.chatService.deleteChat(request.user.id, params.receiverId)
   }
