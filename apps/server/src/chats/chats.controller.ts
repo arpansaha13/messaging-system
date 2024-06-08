dd1657b7485acab93700e36fcb6cc7b1dd9115ee
arpansaha13 <arpansaha1300@gmail.com>
2024-06-08 09:47:37 +0530

    fix(types): user-id-param dto

--- apps/server/src/chats/chats.controller.ts
+++ apps/server/src/chats/chats.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Req, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { ChatsService } from './chats.service'
-import { ChatIdParam } from './dtos/chat-id-param.dto'
+import { UserIdParam } from './dtos/user-id-param.dto'
 import type { Request } from 'express'
 import type { IChatListItem } from '@pkg/types'
 import type { IChatsResponse } from './interfaces/chats-response.interface'
@@ -17,43 +17,43 @@ export class ChatsController {
   }
 
   @Get('/:receiverId')
-  getChatOfUserWithReceiver(@Req() request: Request, @Param() params: ChatIdParam): Promise<IChatListItem> {
+  getChatOfUserWithReceiver(@Req() request: Request, @Param() params: UserIdParam): Promise<IChatListItem> {
     return this.chatService.getChatOfUserWithReceiver(request.user.id, params.receiverId)
   }
 
   @Patch('/:receiverId/archive')
   @HttpCode(HttpStatus.NO_CONTENT)
-  archiveRoom(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
+  archiveRoom(@Req() request: Request, @Param() params: UserIdParam): Promise<void> {
     return this.chatService.updateArchive(request.user.id, params.receiverId, true)
   }
 
   @Patch('/:receiverId/unarchive')
   @HttpCode(HttpStatus.NO_CONTENT)
-  unarchiveRoom(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
+  unarchiveRoom(@Req() request: Request, @Param() params: UserIdParam): Promise<void> {
     return this.chatService.updateArchive(request.user.id, params.receiverId, false)
   }
 
   @Patch('/:receiverId/pin')
   @HttpCode(HttpStatus.NO_CONTENT)
-  pinChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
+  pinChat(@Req() request: Request, @Param() params: UserIdParam): Promise<void> {
     return this.chatService.updatePin(request.user.id, params.receiverId, true)
   }
 
   @Patch('/:receiverId/unpin')
   @HttpCode(HttpStatus.NO_CONTENT)
-  unpinChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
+  unpinChat(@Req() request: Request, @Param() params: UserIdParam): Promise<void> {
     return this.chatService.updatePin(request.user.id, params.receiverId, false)
   }
 
   @Delete('/:receiverId/clear')
   @HttpCode(HttpStatus.NO_CONTENT)
-  clearChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
+  clearChat(@Req() request: Request, @Param() params: UserIdParam): Promise<void> {
     return this.chatService.clearChat(request.user.id, params.receiverId)
   }
 
   @Delete('/:receiverId/delete')
   @HttpCode(HttpStatus.NO_CONTENT)
-  deleteChat(@Req() request: Request, @Param() params: ChatIdParam): Promise<void> {
+  deleteChat(@Req() request: Request, @Param() params: UserIdParam): Promise<void> {
     return this.chatService.deleteChat(request.user.id, params.receiverId)
   }
 }
