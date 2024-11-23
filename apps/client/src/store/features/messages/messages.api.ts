8759aedb99b7a13515ee9a2d1a2e30739577398e
arpansaha13 <arpansaha1300@gmail.com>
2024-11-23 12:46:42 +0530

    fix: types

--- apps/client/src/store/features/messages/messages.api.ts
+++ apps/client/src/store/features/messages/messages.api.ts
@@ -1,12 +1,12 @@
 import _fetch from '~/utils/api/_fetch'
-import type { IMessage } from '@shared/types'
+import type { IGroupMessage, IMessage } from '@shared/types'
 import type { IChannel, IUser } from '@shared/types/client'
 
 export function _getMessages(receiverId: IUser['id']): Promise<IMessage[]> {
   return _fetch(`messages/${receiverId}`)
 }
 
-export function _getGroupMessages(channelId: IChannel['id']): Promise<IMessage[]> {
+export function _getGroupMessages(channelId: IChannel['id']): Promise<IGroupMessage[]> {
   return _fetch(`messages/channel/${channelId}`)
 }
 
